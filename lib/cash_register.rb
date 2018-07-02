12fcf74823ab7f43fea7c77093e8d806cfb4ee97
Emily Wessel <ettl02@gmail.com>
2018-07-02 19:21:09 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,23 +1,19 @@
-# 
-# class CashRegister
-#   attr_accessor :new
-# end
+class CashRegister
+  attr_accessor :items, :discount, :total, :last_transaction 
+end
 
 
-# def initialize (total)
-#@total = total 
-#end 
-# def total
-# end
-#
-# def add_item (title, price)
-# end
-#
-# def apply_discount
-# end
-#
-# def items
-# end
-#
-# def void_last_transaction
-# end
+def initialize (discount = 0)
+@total = 0
+end
+
+
+def add_item (title, price)
+end
+
+def apply_discount
+end
+
+
+def void_last_transaction
+end
