a5b9802f6ffb1ff8df93a50492bb0d03274944fa
Emily Wessel <ettl02@gmail.com>
2018-07-02 19:15:32 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,20 +1,20 @@
-
-class CashRegister
-  attr_accessor :new
-end 
-
-
-def total 
-end 
-
-def add_item 
-end 
-
-def apply_discount 
-end
-
-def items 
-end 
-
-def void_last_transaction
-end 
+# 
+# class CashRegister
+#   attr_accessor :new
+# end
+# 
+# 
+# def total
+# end
+# 
+# def add_item
+# end
+# 
+# def apply_discount
+# end
+# 
+# def items
+# end
+# 
+# def void_last_transaction
+# end
